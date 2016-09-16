9e1ead9aa3850ba2ee0844ceb445a62418bbf56d
Jonathan Islas Garcia <islas.27@gmail.com>
2016-09-16 11:48:32 -0600

    Added solution for GCJ Qualification Round 2015 Problem C (Dijkstra) in Java
    -- Removed duplicated files
    -- Added README for how to use

---
+++ solutions/dijkstra/java/Dijkstra.java
@@ -0,0 +1,146 @@
+import java.util.Scanner;
+
+public class Dijkstra {
+
+    private static int L;
+    private static long X;
+    private static String block;
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        final int T = input.nextInt();
+
+        for (int caseNum = 1; caseNum <= T; caseNum++) {
+
+            L = input.nextInt();
+            X = input.nextLong();
+            block = input.next();
+
+            Quaternion blockQuaternion = Quaternion.get('1');
+            for (int i = 0; i < L; i++) {
+                blockQuaternion = multiply(blockQuaternion, Quaternion.get(block.charAt(i)));
+            }
+
+            int xX = (int) (X % 4);
+            boolean isPossible = false;
+
+            Quaternion tmp = Quaternion.get('1');
+            for (int i = 0; i < xX; i++) {
+                tmp = multiply(tmp, blockQuaternion);
+            }
+
+            Quaternion negOne = new Quaternion(true, '1');
+            if (tmp.equals(negOne)) {
+                // Further check
+                isPossible = isPossible();
+            }
+
+            String pos = (isPossible ? "YES" : "NO");
+
+            String result = String.format("Case #%d: %s", caseNum, pos);
+            System.out.println(result);
+        }
+    }
+
+    public static Quaternion multiply(Quaternion i, Quaternion j) {
+        boolean sign = i.sign ^ j.sign;
+        char value = '1';
+
+        switch (i.value) {
+            case '1':
+                value = j.value;
+                break;
+            case 'i':
+                switch (j.value) {
+                    case '1':
+                        value = 'i';
+                        break;
+                    case 'i':
+                        sign = !sign;
+                        value = '1';
+                        break;
+                    case 'j':
+                        value = 'k';
+                        break;
+                    case 'k':
+                        sign = !sign;
+                        value = 'j';
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case 'j':
+                switch (j.value) {
+                    case '1':
+                        value = 'j';
+                        break;
+                    case 'i':
+                        sign = !sign;
+                        value = 'k';
+                        break;
+                    case 'j':
+                        sign = !sign;
+                        value = '1';
+                        break;
+                    case 'k':
+                        value = 'i';
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case 'k':
+                switch (j.value) {
+                    case '1':
+                        value = 'k';
+                        break;
+                    case 'i':
+                        value = 'j';
+                        break;
+                    case 'j':
+                        sign = !sign;
+                        value = 'i';
+                        break;
+                    case 'k':
+                        sign = !sign;
+                        value = '1';
+                        break;
+                    default:
+                        break;
+                }
+                break;
+        }
+        Quaternion q = new Quaternion(sign, value);
+        return q;
+    }
+
+    private static boolean isPossible() {
+
+        Quaternion left = Quaternion.get('1');
+        Quaternion right = Quaternion.get('1');
+
+        for (int i = 0; i < Math.min(4 * L, L * X); i++) {
+            left = multiply(left, Quaternion.get((block.charAt(i % L))));
+            if (isValidFirst(left)) {
+                for (long j = L * X - 1; j >= Math.max(L * X - 4 * L, i); j--) {
+                    right = multiply(Quaternion.get(block.charAt((int) (j % L))), right);
+                    if (isValidLast(right)) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isValidFirst(Quaternion first) {
+        return first.equals(Quaternion.get('i'));
+    }
+
+    private static boolean isValidLast(Quaternion last) {
+        return last.equals(Quaternion.get('k'));
+    }
+}
---
+++ solutions/dijkstra/java/README.md
@@ -0,0 +1,7 @@
+# Instructions to run program
+
+```sh
+javac quaternion.java
+javac dijkstra.java
+java dijkstra <input.txt >output.txt
+```
