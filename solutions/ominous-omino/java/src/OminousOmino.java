db2c4466678b4091380731f64db22371e45b274a
francisco neri <on_neri@hotmail.com>
2017-10-19 16:09:38 -0500

    correcciones

--- solutions/ominous-omino/java/src/OminousOmino.java
+++ solutions/ominous-omino/java/src/OminousOmino.java
@@ -8,9 +8,9 @@ public class OminousOmino {
     public String evaluateInput(List<String> input){
         String result = "";
         try {
-            int t = Integer.parseInt(input.remove(0)); // t = total cases
-            if(t>=1 && t<=100){ //verify that the input is on the limit of the program
-                if(t == input.size()){ //verify that the number of test cases given is equal to the t
+            int t = Integer.parseInt(input.remove(0));  // t = total cases
+            if(t>=1 && t<=100){                         //verify that the input is on the limit of the program
+                if(t == input.size()){                  //verify that the number of test cases given is equal to input size
                     for(int i=0; i<t; i++){
                         String testCase[] = input.get(i).split(" ");
                         result+="Case #"+(i+1)+": "+evaluateCase(testCase)+"\n";
@@ -18,7 +18,7 @@ public class OminousOmino {
                 }else{
                     System.out.println("Error: the number of test cases given doesn't match with the number received");
                 }
-            }else{ // if t is off the limits then we put an error
+            }else{
                 System.out.println("Error: this input exceed the number of test cases accepted");
             }
         } catch (NumberFormatException e) {
@@ -26,6 +26,12 @@ public class OminousOmino {
         }
         return result;
     }
+
+    /**
+     *
+     * @param testCase string line that has the values to evaluate
+     * @return name of winner
+     */
     private String evaluateCase(String[] testCase){
         String winner = "GABRIEL";
         int xomino=0,rows,columns, area, xominoMaxRows,xominoMaxColumns;
@@ -35,7 +41,7 @@ public class OminousOmino {
             rows = Integer.parseInt(testCase[1]);
             columns = Integer.parseInt(testCase[2]);
             area = rows*columns;
-            if (xomino > area || area%xomino >0  || xomino>6 || perfectPieceExists(xomino,rows,columns) ) {
+            if (xomino>6 || xomino > area || area%xomino >0 || perfectPieceExists(xomino,rows,columns) ) {
                 winner = "RICHARD";
             }
         }catch(NumberFormatException e){
@@ -45,14 +51,24 @@ public class OminousOmino {
 
         return winner;
     }
+
+    /**
+     * find if there is a piece that guarantees is impossible to fill the area
+     * @param xomino omino number
+     * @param rows rows in area
+     * @param columns columns in area
+     * @return
+     */
     private boolean perfectPieceExists(int xomino,int rows,int columns){
         int length=xomino,height=1;
 
+        //special cases for omino 4 that always block the area to be filled
         if(xomino==4) {
             if(rows == 2 && columns >2 || columns == 2 && rows >2){
                 return true;
             }
         }
+        //special cases for omino 5 that always block the area to be filled
         if(xomino==5) {
             if(rows == 2 || columns == 2 ||
                     rows == 3 && columns ==5 ||
@@ -60,6 +76,7 @@ public class OminousOmino {
                 return true;
             }
         }
+        //special cases for omino 6 that always block the area to be filled
         if(xomino==6) {
             if(rows == 2 && columns >3 || columns == 2 && rows >3 ||
                     rows == 3 || columns == 3 ||
