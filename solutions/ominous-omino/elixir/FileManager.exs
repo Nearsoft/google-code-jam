f1beaf623c6150ef6aba08bb7a1df614cec6b965
Haydee Rodriguez <hrodriguez@nearsoft.com>
2017-10-21 03:15:10 -0500

    WIP changes for the FileManager whe it does the methods publics or privates

--- solutions/ominous-omino/elixir/FileManager.exs
+++ solutions/ominous-omino/elixir/FileManager.exs
@@ -1,13 +1,13 @@
 defmodule FileManager do
-
-  def readFile (file) do
-    case File.read(file) do
-      {:ok, body}-> body |> toList 
-      {:error, reason}-> "Failed to open file, error: #{reason}"
+  #Method wo read the file
+  def readFile (file)do
+    case File.read(file) do #in case one
+      {:ok, body}      -> body |> toList #do something with the body
+      {:error, reason} -> "Failed to open file, error: #{reason}"# handle the error caused by `reason`
     end
   end
 
-  def toList(string) do
+  defp toList(string) do
     string
     |> String.trim(" ")
     |> String.split("\n")
@@ -21,19 +21,18 @@ defmodule FileManager do
     end
   end
 
+  #This method write in the file the content of the result
   def writeFile(list) do
-   
     case is_list(list) do
       true -> stringToWrite = list |> Enum.join ("\n")
-        case File.write("results.txt", stringToWrite)  do
-          :ok -> case File.cwd()  do
+      case File.write("results.txt", stringToWrite)  do
+        :ok -> case File.cwd()  do
             {:ok, cwd} -> "Saved results to #{cwd}/results.txt"
             {:error, reason} -> "Failed to save. Error #{reason}"
           end
-          {:error, reason} -> "Failed to save. Error #{reason}"
-        end
+        {:error, reason} -> "Failed to save. Error #{reason}"
+      end
       false -> "Cannot write file, did you pass a list?"
     end
   end
-  
 end
