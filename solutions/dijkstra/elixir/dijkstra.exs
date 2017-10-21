c3301c96da65ad7af03cd6daa1df9db18607dcff
Ramon Jimenez <ejimenez@nearsoft.com>
2017-10-20 20:33:41 -0700

    created filemanagement module

---
+++ solutions/dijkstra/elixir/dijkstra.exs
@@ -0,0 +1,11 @@
+defmodule dijkstra do
+  @multiplicationMatrix {"11"=>"1","1i"=>"i","1j"=>"j","1k"=>"k",
+                         "i1"=>"i","ii"=>"-1","ij"=>"k","ik"=>"-j",
+                         "j1"=>"j","ji"=>"-k","jj"=>"-1","jk"=>"i",
+                         "k1"=>"k","ki"=>"j","kj"=>"-i","kk"=>"-1"}
+
+  def evaluate_input(input) do
+    result=""
+        
+  end
+end
