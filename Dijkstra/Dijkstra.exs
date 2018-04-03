a0ae21eaad1fc530038e94e44f485fb67262c1a8
Ruben Dario Araiza Acosta <raraiza@nearsoft.com>
2018-04-02 20:41:14 -0500

    puto el que lo lea

--- Dijkstra/Dijkstra.exs
+++ Dijkstra/Dijkstra.exs
@@ -17,7 +17,8 @@ defmodule Dijkstra do
         {l, _} = Integer.parse(ll)
         {x, _} = Integer.parse(xx)
         word = String.duplicate(input2, x)
-
+        _ = numberOfCase
+        _ = l
         IO.puts word
         
         # Cosas de evaluacion de las cadenas
@@ -27,25 +28,46 @@ defmodule Dijkstra do
 end
 
 defmodule Quaternion do
-   def mult(a, b) do
-       if hd(a) 
-        ["1", "i", "j", "k", "-1", "-i", "-j", "-k"]
-      tabla =  [["1", "i", "j", "k", "-1", "-i", "-j", "-k"],
-                ["i", "-1", "k", "-j", "-i", "1", "-k", "j"],
-                ["j", "-k", "-1", "i", "-j", "k", "1", "-i"],
-                ["k", "j", "-i", "-1", "-k", "-j", "i", "1"],
-                ["-1", "-i", "-j", "-k", "1", "i", "j", "k"],
-                ["-i", "1", "-k", "j", "i", "-1", "k", "-j"],
-                ["-j", "k", "1", "-i", "j", "-k", "-1", "i"],
-                ["-k", "-j", "i", "1", "k", "j", "-i", "-1"]]
+    def mult(a, b) do
+       indx = String.to_atom(a)
+       indy = String.to_atom(b)
        
-       Enum.at(Enum.at(tabla, 0),0)
+       mult = %{:"1" => %{:"1" => "1", :"i" => "i", :"j" => "j", :"k" => "k", :"-1" => "-1", :"-i" => "-i", :"-j" => "-j", :"-k" => "-k"},
+                :"i" => %{:"1" => "i", :"i" => "-1", :"j" => "k", :"k" => "-j", :"-1" => "-i", :"-i" => "1", :"-j" => "-k", :"-k" => "j"},
+                :"j" => %{:"1" => "j", :"i" => "-k", :"j" => "-1", :"k" => "i", :"-1" => "-j", :"-i" => "k", :"-j" => "1", :"-k" => "-i"},
+                :"k" => %{:"1" => "k", :"i" => "j", :"j" => "-i", :"k" => "-1", :"-1" => "-k", :"-i" => "-j", :"-j" => "i", :"-k" => "1"},
+                :"-1" => %{:"1" => "-1", :"i" => "-i", :"j" => "-j", :"k" => "-k", :"-1" => "1", :"-i" => "i", :"-j" => "j", :"-k" => "k"},
+                :"-i" => %{:"1" => "-i", :"i" => "1", :"j" => "-k", :"k" => "j", :"-1" => "i", :"-i" => "-1", :"-j" => "k", :"-k" => "-j"},
+                :"-j" => %{:"1" => "-j", :"i" => "k", :"j" => "1", :"k" => "-i", :"-1" => "j", :"-i" => "-k", :"-j" => "-1", :"-k" => "i"},
+                :"-k" => %{:"1" => "-k", :"i" => "-j", :"j" => "i", :"k" => "1", :"-1" => "k", :"-i" => "j", :"-j" => "-i", :"-k" => "-1"}}
+    
+        pre = mult[indx]
+        pre[indy]
+    end
+    
+    def reduceWordTo1(word) do
+        
+        if String.length(word) > 1 do
+            h = String.at(word, 0)
+            t = String.slice(word, 1..-1) 0r
 
-   end
+            mult(h, reduceWordTo1(t))
+        else 
+            word
+        end
+    end
+
+    def seekijk(word) do
+        word
+    end
 end
 
-#Recogemos los datos de usuario
-{tests, _} = Integer.parse(IO.gets "hom many times? ")
+##Recogemos los datos de usuario
+#{tests, _} = Integer.parse(IO.gets "hom many times? ")
+#
+## Corremos la funcion tests veces 
+#Dijkstra.run_test(1, tests)
 
-# Corremos la funcion tests veces 
-Dijkstra.run_test(1, tests)
+
+IO.puts Quaternion.seekijk("ijk")
+
