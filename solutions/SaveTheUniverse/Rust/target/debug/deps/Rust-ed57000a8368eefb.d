9fea3908e52bdf4c3c087fdaaeeee5edbe512f7d
Cesar Villalobos <cvillalobos@nearsoft.com>
2019-03-04 13:15:37 -0700

    Solution in Rust language

---
+++ solutions/SaveTheUniverse/Rust/target/debug/deps/Rust-ed57000a8368eefb.d
@@ -0,0 +1,5 @@
+/home/nsl-cvillalobos/VS Code/google-code-jam/solutions/SaveTheUniverse/Rust/target/debug/deps/Rust-ed57000a8368eefb: src/main.rs
+
+/home/nsl-cvillalobos/VS Code/google-code-jam/solutions/SaveTheUniverse/Rust/target/debug/deps/Rust-ed57000a8368eefb.d: src/main.rs
+
+src/main.rs:
