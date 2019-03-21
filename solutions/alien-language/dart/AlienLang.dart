e68f8ee227344d981718e27d17cf8cebacd68b98
daniel grijalva <jdgs.gt@gmail.com>
2019-03-20 19:41:02 -0700

    Renamed files

--- solutions/alien-language/dart/AlienLang.dart
+++ solutions/alien-language/dart/AlienLang.dart
@@ -1,70 +1,34 @@
 import 'dart:io';
-import 'dart:convert';
-import 'dart:async';
 
+main() {
+  final filename = 'solution-small.txt';
+  var sink = new File(filename).openWrite();
 
+  new File('A-small-practice.in').readAsLines().then((List<String> lines) {
+    var fileVariables = lines[0].split(" "), regExpr = [];
+    int wordLength = int.parse(fileVariables[0]), languageLength = int.parse(fileVariables[1]), testCases = int.parse(fileVariables[2]); //L D N
+    
+    var languageWords = [];
+    var solutions = new List(testCases);
 
-List<String> list;
-int wordsNumber;
-int wordsLength;
-int testCasesNumber;
-
-void write(String message){
-  var file = new File('file.txt');
-  var sink = file.openWrite();
-  sink.write(message);
-  sink.close();
-}
-
-String main(){
-  List<String> dictionary = new List();
-  List<String> lines = new File('A-large-practice.in').readAsLinesSync();
-  List<String> regexList = new List();
-  var parametersString = lines[0];
-  var parametersList = parametersString.split(' ');
-  wordsLength = int.parse(parametersList[0]);
-  wordsNumber = int.parse(parametersList[1]);
-  testCasesNumber = int.parse(parametersList[2]);
-  print (parametersList);
-  print (wordsLength);
-  print (wordsNumber);
-  print (testCasesNumber);
-  var file = new File('file.txt');
-  var sink = file.openWrite();
-
-  int i = 1;
-  int testCaseCounter = 0;
-  lines.removeAt(0);
-  for (var line in lines) {
-    if(i <= wordsNumber){
-      dictionary.add(line);
-    }if(i>wordsNumber && i < testCasesNumber + wordsNumber + 1){
-      int testSuccessCounter = 0;
-      testCaseCounter++;
-      String regularExp = changeToRegEx(line);
-      regexList.add(regularExp);
+    for(int i=0; i<languageLength; i++){ //Add the language that will be tested.
+      languageWords.add(lines[i+1]);
     }
-    i++;
-  }
-  int j = 1;
-  for(var regex in regexList){
-    int count = 0;
-    for(var word in dictionary){
-      //print ("$word  --  $regex");
-      if(word.contains(new RegExp(regex))){
-        count ++;
+
+    for(int i=0; i<testCases; i++){ //Iterate through each String regex.
+      regExpr.add(lines[i+languageLength+1]);
+      regExpr[i] = regExpr[i].replaceAll("(","[").replaceAll(")","]");
+    }
+
+    for(int r = 0; r < regExpr.length; r++){
+      regExpr[r] = new RegExp(regExpr[r]);
+      solutions[r]=0;
+      for(int t = 0; t <languageWords.length; t++){
+        if(regExpr[r].hasMatch(languageWords[t])){
+          solutions[r]++;
+        }
       }
+      sink.write("Case #${r+1}: ${solutions[r]}\n");
     }
-    //print (regexList.length);
-    print("Case #$j: $count");
-    sink.write("Case #$j: $count\n");
-    j++;
-  }
-  sink.close();
-}
-
-String changeToRegEx(String line){
-  line = line.replaceAll(new RegExp(r'\('), '[');
-  line = line.replaceAll(new RegExp(r'\)'), ']');
-  return line;
+  });
 }
