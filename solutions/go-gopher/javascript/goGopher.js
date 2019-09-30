90c1c9eaecfbd7d338e552097276652549855fa7
Jorge Cordoba <jcordoba@NSL-JCORDOBAs-MacBook-Pro.local>
2019-09-30 11:51:41 -0700

    Working solution for GoGopher in Node.js

--- solutions/go-gopher/javascript/goGopher.js
+++ solutions/go-gopher/javascript/goGopher.js
@@ -1,37 +1,12 @@
-var random = Math.random()
 const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-});
-function calculateHit(targets, x, y){
-    var done = true
-    for (var key in targets) {
-        if (targets[key] > 0) {
-            done = false
-        }
-    }
-    if (done) {
-        return "0 0"
-    }
+    });
 
-    var myX = Math.floor((Math.random() * 3) + (x-1))
-    var myY = Math.floor((Math.random() * 3) + (y-1))
-    return myX.toString() + " " + myY.toString()
-    
-}
-
-// function randomIntFromInterval(min, max) { // min and max included
-//     return Math.floor(Math.random() * (max - min + 1) + min);
-// }
-
-
-    
-
+// Function to update our dictionary of best possible shots
 function updateTargets(targets, x, y){
-
     var keys = Object.keys(targets)
-
     for (var target in keys) {
         tX = parseInt(keys[target].split(",")[0])
         tY = parseInt(keys[target].split(",")[1])
@@ -43,10 +18,22 @@ function updateTargets(targets, x, y){
 }
 // # Finds the optimal dimentions for a rectangle that can fit 'a' inside
 function findSquareDimentions(a, n, m){
+    // # Create a map of the places where we can deploy the gopher
+    // # and assign them a starting scores of the number of squares
+    // # that haven't been marked yet (9)
+    // # For example, for a = 16 we would get the dimensions of x=4 and y=4
+    // #   1 2 3 4 5 6
+    // # 1|0 0 0 0 0 0
+    // # 2|0 x x x 0 0 
+    // # 3|0 x x x 0 0 
+    // # 4|0 0 0 0 0 0 
+    // # 5|0 0 0 0 0 0
+    // # the places where 'x' it's the only places we would want to shoot, notice how the 0's
+    // # surrounding the x's form the 4x4 square that we want to fill up
     var smallestDifference = a
-
     for (let l = 0; l < Math.floor(a); l++) {
-
+        // We just keep the combination of 'n' and 'm' that multiplied together equal A and the difference between
+        // 'n' and 'm' its the lowest
         if(a % l === 0 && Math.abs(l-Math.floor(a/l)) < smallestDifference){
             smallestDifference = Math.abs(l-Math.floor(a/l))
             n = l
@@ -58,111 +45,108 @@ function findSquareDimentions(a, n, m){
     }
     return [n,m]
 }
-async function main(){
-    // var blabla = await readLinePromise()
-    // console.log(blabla)
-    var t = await readLinePromise()
-    // console.log('t is: ' + t)
-    t = parseInt(t)
-    var total = 0
-    for (let i = 0; i < t; i++) {
-        var tiros = 0
-        var a = parseInt(await readLinePromise())
-        // console.log('a is: ' + a)
-        var n
-        var m
-        var dimentionsOfSquare = []
-        dimentionsOfSquare = findSquareDimentions(a, n, m)
+
+
+// 'T' its the number of test cases the tester will iterate
+let T = 0;
+rl.question('', answer => {
+T = parseInt(answer);
+var i = 0;
+
+function do_process() {
+    if (i >= T) {
+        console.error(i)
+        // Process exit will terminate iteration and begin with the next test case
+        process.exit();
+        return;
+    }
+    var targets = {}
+    var n = 0
+    var m = 0
+    var dimentionsOfSquare = []
+    // Make a list for the places that are already mark so we don't count them twice
+    var alreadyPrepared = []
+    
+    process_case(
+    (A) => {
+        dimentionsOfSquare = findSquareDimentions(A, n, m)
         n = dimentionsOfSquare[0]
         m = dimentionsOfSquare[1]
-        // # Create a map of the places where we can deploy the gopher
-        // # and assign them a starting scores of the number of squares
-        // # that haven't been marked yet (9)
-        // # For example, for a = 16 we would get the dimensions of x=4 and y=4
-        // #   1 2 3 4 5 6
-        // # 1|0 0 0 0 0 0
-        // # 2|0 x x x 0 0 
-        // # 3|0 x x x 0 0 
-        // # 4|0 0 0 0 0 0 
-        // # 5|0 0 0 0 0 0
-    
-        // # the places where 'x' it's the only places we would want to shoot, notice how the 0's
-        // # surrounding the x's form the 4x4 square that we want to fill up
-    
-    
-        var targets = {}
         for (let h = 2; h < n; h++) {
-    
             for (let k = 2; k < m; k++) {
-    
                 targets[[h,k]] = 9
             }
         }
-        var x = -1
-        var y = -1
-        
-        // # Make a list for the places that are already mark so we don't count them twice
-        var alreadyPrepared = []
-        // Start iterating, shooting and updating the map, until we get 0 0 which signals the end of the test case
-        while (!(x === 0 && y === 0)) {
-            // Get best target from map
-            var highestMissingSquares = 0
-            var nextTarget  
-                      
-            for (var key in targets) {
-    
-                if (targets[key] > highestMissingSquares) {
-    
-                    nextTarget = key
-                    highestMissingSquares = targets[key]
-                }
+    },
+    () => {
+        // Function to pick the shot where it has a higher chance to hit an unpainted tile within A
+        var highestMissingSquares = 0
+        var nextTarget = ""
+        for (var key in targets) {
+            if (targets[key] > highestMissingSquares) {
+                nextTarget = key
+                highestMissingSquares = targets[key]
             }
-    
-            
-            var result // '1 1'
-            var targetX = nextTarget.split(",")[0]
-            var targetY = nextTarget.split(",")[1]
-            // result = calculateHit(targets, targetX, targetY) // random shot
-            console.log(targetX + " " + targetY)
-            result = await readLinePromise()
-            tiros++
-            x = parseInt(result.split(" ")[0])
-            y = parseInt(result.split(" ")[1])
-            lastShot = [x.toString() + ' ' + y.toString()]
-    
-    
-    
-            if (!(alreadyPrepared.includes(lastShot.toString()))) { 
-                // alreadyPrepared.push([x,y])
-                alreadyPrepared.push(lastShot.toString())
-                updateTargets(targets, x, y)
-            }
-    
         }
-        total+=tiros
-        // console.log('Done!')
-    
-    
-    
-            
+        var targetX = parseInt(nextTarget.split(",")[0]);
+        var targetY = parseInt(nextTarget.split(",")[1]);
+        return [targetX, targetY]
+    },
+    (i, j) => {
+        // Get best target from map
+        // pickTarget()
+        // result = calculateHit(targets, targetX, targetY) // random shot
+        // console.log(targetX + " " + targetY)
+        // result = await readLinePromise()
+        // tiros++
+        // x = parseInt(result.split(" ")[0])
+        // y = parseInt(result.split(" ")[1])
+        x = parseInt(i)
+        y = parseInt(j)
+        lastShot = [x.toString() + ' ' + y.toString()]
+        if (!(alreadyPrepared.includes(lastShot.toString()))) { 
+            // alreadyPrepared.push([x,y])
+            alreadyPrepared.push(lastShot.toString())
+            updateTargets(targets, x, y)
+        }
+    }, 
+    do_process
+    );
+    i++
+}
+
+// Initiates Test case
+do_process();
+});
+
+
+
+// Function to execute for every shot we are going to make
+function process_case(select_dimension_callback, select_target_fn, process_result, done) {
+    //This function loops all our shots until we cover al Area we are looking for
+    function gopher_loop() {
+        //Call func to select target to shoot
+        const [i, j] = select_target_fn();
+        //Shoot
+        console.log(`${i} ${j}`)
+        //Read Gopher's painted area
+        rl.question('', line => {
+        var ip = parseInt(line.split(' ')[0])
+        var jp = parseInt(line.split(' ')[1])
+        //If '0 0' then we are done with this test case.
+        if (ip === 0 && jp === 0) {
+            done();
+            return;
+        }
+        process_result(ip, jp);
+        gopher_loop();
+        }); 
     }
-    // console.log(total/t)
-    // console.log('finished.')
-    process.exit()
+
+
+    // Asks question to retrieve Area of the field to paint
+    rl.question('', A => {
+        select_dimension_callback(parseInt(A));
+        gopher_loop();
+    })
 }
-
-
-
-
-function readLinePromise(){
-    return new Promise(function(resolve, reject){
-        rl.question("", function(answer){
-            resolve(answer);
-        });
-    });
-}
-
-
-
-
-main();
