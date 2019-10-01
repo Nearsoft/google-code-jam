f54d1687d3d3605bb6f7c86934cd20505feb6cf3
Gilberto Ayala <gayala@nearsoft.com>
2019-10-01 11:39:48 -0700

    Implementation of troublesort in dart

--- solutions/trouble-sort/dart/implementation.dart
+++ solutions/trouble-sort/dart/implementation.dart
@@ -1,57 +1,67 @@
 import 'dart:io';
+import 'dart:math' as math;
 
 main() {
   String stringCase;
   //List<String> results =[];
   int numberOfCases = int.parse(stdin.readLineSync());
+  stdout.flush();
+  BytesBuilder builder = new BytesBuilder();
   for (int i = 1; i <= numberOfCases; i++) {
-  String size = stdin.readLineSync();
-  String line = stdin.readLineSync();
-  stringCase = line;
-  var splitter = stringCase.split(" ");
-  List<int> lint =splitter.map(int.parse).toList();
-  //print(lint);
-  separateArray(lint);
-  print("Case #${i}: ${checkSorted(lint)}");
+      int size = int.parse(stdin.readLineSync());
+      List<int> lint = new List(size);
+      for (int j = 0; j < size; j++) {
+        int char = stdin.readByteSync();
+        while (char >= 48 && char <= 57) {
+          builder.addByte(char);
+          char = stdin.readByteSync();
+        }
+        lint[j] = int.parse(String.fromCharCodes(builder.takeBytes()));
+      }
+      //print(lint);
+      if(lint.length > 1 && lint.length <= math.pow(10, 9)){
+   print("Case #${i}: ${separateArray(lint)}");
+      stdout.flush();
+  }
+     
   }
   
+  return 0;
 }
 
 separateArray(array){
-  List<int> odds =[];
-  List<int> evens=[]; 
+
+  List<int> odds = new List((array.length / 2).floor());
+  List<int> evens= new List(array.length - odds.length);
   
+  int m, n;
+  m = 0;
+  n = 0;
   for(var i = 0; i<array.length; i++){
     if(i%2==0){
-     evens.add(array[i]);
+     evens[n] = array[i];
+     n++;
     }
     if(i%2!=0){
-      odds.add(array[i]);
+      odds[m] = array[i];
+      m++;
     }
   }
   evens = evens..sort();
   odds = odds..sort();
   var j=0,k=0;
-  for(var i=0; i<array.length;i++){
+  for(var i=0; i<array.length-1;i++){
     if(i%2==0){
-      array[i]=evens[(j)];
+      if (evens[j] > odds[k]) {
+          return i;
+      }
       j++;
     }else{
-      array[i]=odds[k];
+      if (odds[k] > evens[j]) {
+        return i;
+      }
       k++; 
-    }
-  }
-  return array;
-}
-
-checkSorted(arraySorted){
-  var len = arraySorted.length;
-  var i=0;
-  for(i; i<len -1;i++){
-    if(arraySorted[i]>arraySorted[i+1]){
-      return i ;
     }
   }
   return "OK";
 }
-
