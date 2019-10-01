7a4caf93701819bfbf07c8cb180792e3f5f1a3db
hiram <hzamorano@NSL-HZAMORANOs-MacBook-Pro.local>
2019-10-01 11:29:40 -0700

    TroubleSort V2

--- solutions/trouble-sort/kotlin/TroubleSort.kt
+++ solutions/trouble-sort/kotlin/TroubleSort.kt
@@ -1,19 +1,28 @@
-
-
-
 var odds = arrayOf<Int>()
 var evens = arrayOf<Int>()
-
-fun separateArrays(arr:IntArray) {
- // Sort odd and even
- separateArray(arr)
+fun separateArrays(arr:IntArray): IntArray {
+    // Sort odd and even
+    separateArray(arr)
     odds.sort()
     evens.sort()
+    var newArr = IntArray(arr.size)
+    var ie = 0
+    var io = 0
+    for (i in 0 until arr.size){
+        if((i%2 == 0 || i == 0) && ie < evens.size){
+            newArr[i] = evens[ie]
+            ie++
+        }
+        else if(io < odds.size){
+            newArr[i] = odds[io]
+            io++
+        }
+    }
+    return newArr
 }
-
 fun separateArray(arr:IntArray) {
     for(i in 0 until arr.size){
-        if(i%2 == 0){
+        if(i%2 == 0 || i == 0){
             evens+=(arr[i])
         }
         else {
@@ -21,110 +30,124 @@ fun separateArray(arr:IntArray) {
         }
     }
 }
-
-fun checkTroubleSort(): Int {
-    if (odds.size == evens.size){
-        for (i in 0 until odds.size) {
-            if (evens[i] > odds[i]) {
-                // return esta mal regresa index
-                return i
-            }
-        }
-        return -1
-    }
-    else if(odds.size > evens.size){
-        for (i in 0 until evens.size) {
-            if (evens[i] > odds[i]) {
-                // return esta mal regresa index
-                return i
-            }
-        }
-        return -1
-    }
-    else{
-        for (i in 0 until odds.size) {
-            if (evens[i] > odds[i]) {
-                // return esta mal regresa index
-                return i
-            }
-        }
-        return -1
-    }
-//    for (i in 0 until len - 1) {
-//        if (evens[i] > odds[i]) {
-//            // return esta mal regresa index
-//            return i
-//        }
-//    }
-//    return -1
-}
-
-fun troubleSort2(arr:IntArray){
-    var cambio = false
-    for ((cont, i) in arr.withIndex()){
-        if(cont+2 < arr.size){
-            if (arr[cont] > arr[cont+2]){
-                var aux = arr[cont]
-                arr[cont] = arr[cont+2]
-                arr[cont+2] = aux
-                cambio = true
-            }
+fun checkForErrors(arr:IntArray): Int{
+    for (i in 0 until arr.size - 1){
+        if (arr[i] > arr[i+1]){
+            return i
         }
     }
-    if (cambio){
-        troubleSort2(arr)
-    }
+    return -1
 }
 fun main(args: Array<String>) {
-
-    var t = readLine()!!.toInt()
+    val reader = System.`in`.bufferedReader()
+    var t = reader.readLine()!!.toInt()
     var testCase = 1
     var resultados = listOf<String>()
     while (t > 0) {
         // Pedir tamaño de arreglo
-        var n = readLine()!!.toInt()
-        var flag = false
+        var n = reader.readLine()!!.toInt()
+        //reset odds and evens
+        odds = arrayOf<Int>()
+        evens = arrayOf<Int>()
         // Llenar el arreglo donde el input es un string y cada numero esta separado por espacios
-        var values = readLine()!!.toString()
+        var values = reader.readLine()!!.toString()
         var mappedValues = values.split(' ').map { it.toInt() }
         var primeArray = IntArray(n)
         for ((cont, i) in mappedValues.withIndex()){
             primeArray[cont] = i
         }
-
         // Separate odd even
-        separateArrays(primeArray)
-        // Compare odd[i] with even[i]
-        var result = checkTroubleSort()
+        var resultArr = separateArrays(primeArray)
+        // Compare if its sorted correctly
+        var result = checkForErrors(resultArr)
         // if error its found return i
         // else iterate through original list and compare:
         if (result == -1) {
-            // arr[i] > arr[i+1]
-            // if true return i
-            // else all good
-            troubleSort2(primeArray)
-            for (x in 0 until primeArray.size - 1){
-                if (primeArray[x] > primeArray[x+1]){
-                    flag = true
-                    resultados += "CASE #$testCase: $x"
-                    break
-                }
-            }
-        }
-        if (!flag){
             resultados += "CASE #$testCase: OK"
         }
-
-
-
-        //End Rest----------
-
+        else{
+            resultados += "CASE #$testCase: $result"
+        }
         t -= 1
         testCase += 1
     }
     for (i in resultados){
         println(i)
     }
-
+}var odds = arrayOf<Int>()
+var evens = arrayOf<Int>()
+fun separateArrays(arr:IntArray): IntArray {
+    // Sort odd and even
+    separateArray(arr)
+    odds.sort()
+    evens.sort()
+    var newArr = IntArray(arr.size)
+    var ie = 0
+    var io = 0
+    for (i in 0 until arr.size){
+        if((i%2 == 0 || i == 0) && ie < evens.size){
+            newArr[i] = evens[ie]
+            ie++
+        }
+        else if(io < odds.size){
+            newArr[i] = odds[io]
+            io++
+        }
+    }
+    return newArr
 }
+fun separateArray(arr:IntArray) {
+    for(i in 0 until arr.size){
+        if(i%2 == 0 || i == 0){
+            evens+=(arr[i])
+        }
+        else {
+            odds+=(arr[i])
+        }
+    }
+}
+fun checkForErrors(arr:IntArray): Int{
+    for (i in 0 until arr.size - 1){
+        if (arr[i] > arr[i+1]){
+            return i
+        }
+    }
+    return -1
+}
+fun main(args: Array<String>) {
+    val reader = System.`in`.bufferedReader()
+    var t = reader.readLine()!!.toInt()
+    var testCase = 1
+    var resultados = listOf<String>()
+    while (t > 0) {
+        // Ask Array size
+        var n = reader.readLine()!!.toInt()
+        //reset odds and evens
+        odds = arrayOf<Int>()
+        evens = arrayOf<Int>()
 
+        var values = reader.readLine()!!.toString()
+        var mappedValues = values.split(' ').map { it.toInt() }
+        var primeArray = IntArray(n)
+        for ((cont, i) in mappedValues.withIndex()){
+            primeArray[cont] = i
+        }
+        // Separate odd even
+        var resultArr = separateArrays(primeArray)
+        // Compare if its sorted correctly
+        var result = checkForErrors(resultArr)
+        // if error its found return i
+        // else iterate through original list and compare:
+        if (result == -1) {
+            resultados += "CASE #$testCase: OK"
+        }
+        else{
+            resultados += "CASE #$testCase: $result"
+        }
+        t -= 1
+        testCase += 1
+    }
+    for (i in resultados){
+        println(i)
+    }
+}
