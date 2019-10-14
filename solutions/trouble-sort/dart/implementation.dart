2f035dc413404599c07acd71abeb6ec04c413aef
Gilberto Ayala <gayala@nearsoft.com>
2019-10-14 10:33:10 -0700

    code now works on google code jam

--- solutions/trouble-sort/dart/implementation.dart
+++ solutions/trouble-sort/dart/implementation.dart
@@ -1,39 +1,32 @@
+import 'dart:async';
 import 'dart:io';
-import 'dart:math' as math;
+import 'dart:convert';
+
+var i = 0;
+int nCases = 0;
+var c = 1;
 
 main() {
-  String stringCase;
-  //List<String> results =[];
-  int numberOfCases = int.parse(stdin.readLineSync());
-  stdout.flush();
-  BytesBuilder builder = new BytesBuilder();
-  for (int i = 1; i <= numberOfCases; i++) {
-      int size = int.parse(stdin.readLineSync());
-      List<int> lint = new List(size);
-      for (int j = 0; j < size; j++) {
-        int char = stdin.readByteSync();
-        while (char >= 48 && char <= 57) {
-          builder.addByte(char);
-          char = stdin.readByteSync();
-        }
-        lint[j] = int.parse(String.fromCharCodes(builder.takeBytes()));
-      }
-      //print(lint);
-      if(lint.length > 1 && lint.length <= math.pow(10, 9)){
-   print("Case #${i}: ${separateArray(lint)}");
-      stdout.flush();
+  var stream = stdin
+    .transform(utf8.decoder)
+    .transform(new LineSplitter())
+    .forEach((l) => {handleCase(l)});
+}
+
+handleCase(String l) {
+  if ((i > 0) && (i % 2 == 0)) {
+    List<int> nums = l.split(" ").map(int.parse).toList();
+   print("Case #${c}: ${separateArray(nums)}");
+   c++;
   }
-     
-  }
-  
-  return 0;
+  i++;
 }
 
 separateArray(array){
-
+  var size = array.length;
   List<int> odds = new List((array.length / 2).floor());
   List<int> evens= new List(array.length - odds.length);
-  
+
   int m, n;
   m = 0;
   n = 0;
@@ -50,7 +43,7 @@ separateArray(array){
   evens = evens..sort();
   odds = odds..sort();
   var j=0,k=0;
-  for(var i=0; i<array.length-1;i++){
+  for(var i=0; i<size-1;i++){
     if(i%2==0){
       if (evens[j] > odds[k]) {
           return i;
