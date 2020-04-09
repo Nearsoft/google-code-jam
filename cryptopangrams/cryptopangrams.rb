c47174d3ab6833a719b6f13433b43df7de081d13
sanlf <sanlf@protonmail.com>
2020-04-09 13:04:09 -0700

    Receive parameters from cli

--- cryptopangrams/cryptopangrams.rb
+++ cryptopangrams/cryptopangrams.rb
@@ -50,73 +50,45 @@ end
 
 ##############################################################################
 
-if __FILE__ == $0
+def test_cryptopangrams()
 
-  # CASE 1
+  # reads first line, the number of cases
+  n_cases = gets.chomp
+  n_cases = n_cases.to_i
 
-  puts "\nSTART CASE 1"
+  # iterate the number of cases to test
+  for i in 1..n_cases
+    # read line with N and L
+    line = gets.chomp
+    # read line with list of integers
+    line2 = gets.chomp
 
-  n = 103
-  l = 31
-  integers = [217, 1891, 4819, 2291, 2987, 3811, 1739, 2491, 4717, 445, 65, 1079, 8383, 5353, 901, 187, 649, 1003, 697, 3239, 7663, 291, 123, 779, 1007, 3551, 1943, 2117, 1679, 989, 3053]
+    # turn line with N and L to array of numbers
+    nl = line.split(" ").map{ |i| i.to_i }
+    # extract N and L from the array
+    n = nl[0]
+    l = nl[1]
 
-  message1 =  cryptopangram(n, l, integers)
-  expected_msg1 = "CJQUIZKNOWBEVYOFDPFLUXALGORITHMS"
+    # turn line with integers into array of numbers
+    integers = line2.split(" ").map{ |i| i.to_i }
 
-  puts ("Expected msg 1: ")
-  puts (expected_msg1)
-  puts ("Calculated msg 1: ")
-  puts (message1)
+    message = cryptopangram(n, l, integers)
 
-  if message1 == expected_msg1
-    puts "Test passed!"
-  else
-    puts "Test failed..."
-  end
-
-  # CASE 2
-
-  puts "\nSTART CASE 2"
-
-  n = 10000
-  l = 25
-  integers = [3292937, 175597, 18779, 50429, 375469, 1651121, 2102, 3722, 2376497, 611683, 489059, 2328901, 3150061, 829981, 421301, 76409, 38477, 291931, 730241, 959821, 1664197, 3057407, 4267589, 4729181, 5335543]
-
-  message2 = cryptopangram(n, l, integers)
-  expected_msg2 = "SUBDERMATOGLYPHICFJKNQVWXZ"
-
-  puts ("Expected msg 2: ")
-  puts (expected_msg2)
-  puts ("Calculated msg 2: ")
-  puts (message2)
-
-  if message2 == expected_msg2
-    puts "Test passed!"
-  else
-    puts "Test failed..."
-  end
-
-  # CASE 3
-
-  puts "\nSTART CASE 3"
-
-  n = 107
-  l = 29
-  integers = [15, 15, 15, 15, 21, 49, 77, 143, 221, 323, 437, 667, 899, 1147, 1517, 1763, 2021, 2491, 3127, 3599, 4087, 4757, 5183, 5767, 6557, 7387, 8633, 9797, 10403]
-
-  message3 = cryptopangram(n, l, integers)
-  expected_msg3 = "ABABACCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-  puts ("Expected msg 3: ")
-  puts (expected_msg3)
-  puts ("Calculated msg 3: ")
-  puts (message3)
-
-  if message3 == expected_msg3
-    puts "Test passed!"
-  else
-    puts "Test failed..."
+    puts "Case ##{i}: #{message}"
   end
 
 end
+
+
+if __FILE__ == $0
+  test_cryptopangrams()
+end
+
+#2
+#103 31
+#217 1891 4819 2291 2987 3811 1739 2491 4717 445 65 1079 8383 5353 901 187 649 1003 697 3239 7663 291 123 779 1007 3551 1943 2117 1679 989 3053
+#10000 25
+#3292937 175597 18779 50429 375469 1651121 2102 3722 2376497 611683 489059 2328901 3150061 829981 421301 76409 38477 291931 730241 959821 1664197 3057407 4267589 4729181 5335543
+#197 29
+#15 15 15 15 21 49 77 143 221 323 437 667 899 1147 1517 1763 2021 2491 3127 3599 4087 4757 5183 5767 6557 7387 8633 9797 10403
 
