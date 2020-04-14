e9d06548112033fd596cf4a254eeb91a5d7d0205
cjimenez <carlos_conejo_21@hotmail.com>
2020-04-14 12:17:59 -0700

    Soluction in Java for Dat Bae

--- solutions/dat-bae/java/Solution.java
+++ solutions/dat-bae/java/Solution.java
@@ -2,54 +2,39 @@ import java.util.*;
 import java.io.*;
 import java.lang.Math;
 
-class Lista {
-    public LinkedList<Stack<String>> IDs;
-    public LinkedList<Stack<String>> Responses;
-  
-    public Lista (int N, int B) {
-        IDs = new LinkedList<>();
-        for (int i = 0; i<N; i++)
-            IDs.add(new Stack<String>());
-        Responses = new LinkedList<>();
-        for (int i = 0; i<(N-B); i++)
-            Responses.add(new Stack<String>());
+class Solution {
+    //que pedo con que les hecho la mano
+  // quien es xd xd ??
+    private static String getPC (String[] arreglo, int n) {
+        String id = "";
+        for (int i = arreglo.length-1; i >= 0; i--) {
+          id += arreglo[i].charAt(n);
+        }
+        return id;
     }
     
-    public String getID(int n) {
-        String id = "";
-        for (int i = IDs.get(n).size()-1; i >= 0; i--) {
-          id += IDs.get(n).get(i);
-        }
-        return id;
-    }
-  
-    public String getResponse(int n) {
-        String id = "";
-        for (int i = Responses.get(n).size()-1; i >= 0; i--) {
-          id += Responses.get(n).get(i);
-        }
-        return id;
-    }
-}
-
-public class Solution {
     public static void main(String[] args) {
         int N; // worker computers
         int B; // workers broken
         int F; // calls to TEST_STORE
         int T; // test cases
+        String[] IDs, Responses;
         Boolean finish = false;
-        Lista l;
         
         Scanner sc = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
         T = Integer.parseInt(sc.nextLine());
         for (int i = 0; i < T && !finish; i++) {
             String readLine = sc.nextLine();
-            N = Integer.parseInt(readLine.split(", ")[0]);
-            B = Integer.parseInt(readLine.split(", ")[1]);
-            F = Integer.parseInt(readLine.split(", ")[2]);
-          
-            l = new Lista(N,B);
+            N = Integer.parseInt(readLine.split(" ")[0]);
+            B = Integer.parseInt(readLine.split(" ")[1]);
+            F = Integer.parseInt(readLine.split(" ")[2]);
+          //   5 2 5
+            IDs = new String[F];
+            Responses = new String[F];
+            for (int f = 0; f < F; f++) {
+                IDs[f] = "";
+                Responses[f] = "";
+            }
             
             String guess = ""; // This will send to guess, ex. 010101
             String answer = ""; // When we know the answer, ex. 0 2
@@ -67,7 +52,7 @@ public class Solution {
                     }
                     guess += d; // 00110011
                     //System.out.println("Guess temporal: "+guess);
-                    l.IDs.get(k).push(""+d);
+                    IDs[j] += ""+d;
                 }
                 System.out.println(guess);
                 response = sc.nextLine(); // Response of the judge, ex. 1101
@@ -75,16 +60,14 @@ public class Solution {
                     finish = true;
                     break;
                 }
-                for (int Oof = 0; Oof < response.length(); Oof++) {
-                  l.Responses.get(Oof).push(""+response.charAt(Oof));
-                }
+                Responses[j] = response;
             }
             if (finish) break;
             // Vamos a comparar
             int O = 0;
-            int r=0;
-            for (; r < l.Responses.size(); r++) {
-              if (!l.getID(r+O).equals(l.getResponse(r))) {
+            int r = 0;
+            for (; r < N-B; r++) {
+              if (!getPC(IDs, r+O).equals(getPC(Responses, r))) {
                 O++;
                 r--;
                 answer += (r+O)+" ";
