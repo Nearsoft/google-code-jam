9cd0224a8727b046095f55951fc901953c24c209
Hernán Cervera <hjcerveram@hotmail.com>
2021-04-19 14:24:40 -0500

    Re-think algorithm

--- solutions/cheating-detection/ruby/approach1.rb
+++ solutions/cheating-detection/ruby/approach1.rb
@@ -1,45 +1,60 @@
 =begin
-Algorithm:
+Algorithm to solve one test case:
+
+  1. Store player answers in an array, where the index number + 1 matches
+     the player number. Let's say this array is named ANSWERS (A). The values
+     in the array are strings.
 
   === Gather questions data ===
-  1. Find top 5% of hard questions.
-     These are questions that at most 0.05 * 100 = 5 players got right.
-     Store the numbers of these questions in a list.
-     * Loop once vertically *
+  2. Estimate the difficulty for each question.
+     Formula:
+       (players_who_got_it_right * -6 / total_players) + 3
+       Example: (40 * -6 / 100) + 3
+     a) Iterate over A checking the ith character of every element of A. The
+        ith character represents the questions number, accumulate in another
+        array (let's call it CORRECT_ANSWERS) the correct answers for each
+        question.
+     b) After this, compute the difficulty of each questions and substitute it
+        in-place in array CORRECT_ANSWERS.
+  3. Find the top hardest questions (extreme questions).
+     These are questions with difficulte greater or equal to 2.0.
+
+  ---Alternate steps 2 and 3 (save memory and compute time)---
+  2. Iterate over A checking the ith character of every element of A. The
+     ith character represents the questions number. Accumulate the number
+     of correct results for such ith question in a variable.
+  3. After the all answers have been checked for the ith question, apply
+     the formula to determine the difficulty of the question. If the
+     difficulty is greater or equal to 2.0, store the number of the question
+     in the array of extreme questions (let's call it EXTREME_QUESTIONS).
 
   === Gather players data ===
-  For each player, an object which represents it exists. The object stores:
-      - Number of correctly answered top 5% hard questions (extreme questions).
-      - Estimated skill level.
-      - Player number.
-  2. Estimate skill level of each player.
-     This estimation is based on the proportion of correct answers, e.g.
-     if a player answered right 7000 out of 10000 questions, then his
-     skill level is equal to 7000 * 10 / 10000 = 7000 / 1000 = 7.
-     I consider skill levels to be in the interval [0, 10].
-     Assign each player object their skill level.
-  3. Count number of correct top 5% hard questions per player. e.g. if
-     only 24 questions were answered by 5 or less people, then player 3
-     might have 6 of those questions right, and so on.
-     Assign each player their number of correctly answered questions.
-     * Loop once their answers string, getting data for both steps 2 and 3. *
-  4. Sort player objects, in descending order, by estimated skill level.
-     Store the sorted player objects in a list.
+  4. Estimate the skill level for each player.
+     Formula:
+       (right_answers * 6 / number_of_questions) - 3
+       Example: (5000 * 6 / 10000) - 3
+     Iterate over each element of A doing the following:
+       a) counting the total number of correctly answered questions, and
+       b) counting the number of correctly answered extreme questions.
+     Store (a) and (b) in arrays, let's call them SCORES and EXTREME_SCORES.
 
-  === Analyse neighbors on extreme questions ===
-  5. For every player, compute the max difference of correctly answered
-     extreme questions in comparison to neighbors with a +-1 skill level.
-     Store every computated value in the object being compared.
-     Example:
-     Total number of extreme questions: 24.
-     Currently compared player: #1; skill level:10.0; correct  ext. q: 21.
-     Player  Skill level  Correct extreme questions  Difference
-     2       10.0         20                          1
-     3       9.2          22                         -1
-     4       9.0          18                          3
-     Max difference: 3.
-  6. Find which player has the biggest max difference of correctly
-     answered extreme questions in comparison to its neighbors.
-     This is the cheater.
+  === Find the cheater ===
+  5. Calculate the estimated amount of extreme questions each player should have
+     answered correctly, use the sigmoid function:
+       1 / (1 + e ^ -(player_skill - 2.0)), where e is Euler's number.
+     For each ith player, store EXTREME_SCORES[i] - estimate in an array, let's
+     call this array DIFFERENCES.
+  6. Find the largest element in the array DIFFERENCES, its index + 1 is the number
+     of the cheating player.
 =end
 
+=begin
+What I need to learn about the programming language:
+  - functions
+  - variables
+  - string manipulation
+  - arithmetic
+  - lists
+  - stdin / stdout
+  - control structures (for, if, etc.)
+=end
