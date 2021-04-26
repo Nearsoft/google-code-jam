c56929177e0c8f27f6fa5e3993873fcee2ee5328
Laura <53280856+laulujan@users.noreply.github.com>
2021-04-26 15:45:16 -0500

    Update Reversort.rb
    Updated variables names and add comments

--- solutions/reversort/Ruby/Reversort.rb
+++ solutions/reversort/Ruby/Reversort.rb
@@ -1,5 +1,5 @@
 #Define reverse function
-def rev(arr, i, min_idx)
+def reverse(arr, i, min_idx)
     while i <= min_idx
         tmp = arr[i]
         arr[i] = arr[min_idx]
@@ -10,29 +10,32 @@ def rev(arr, i, min_idx)
   
     arr
 end
-  
+
+#Get number of tests set them to int
 T = gets.to_i
-  
+#For each test get the number of elements in the list, get the list and convert it to array of ints.
 [*1..T].each do |t|
-    N = gets.to_i
-    l = gets.split(' ').map(&:to_i)
+    NumElm = gets.to_i
+    list = gets.split(' ').map(&:to_i)
     cost = 0
-  
-    [*1..N-1].each do |x|
-      i = x-1
+    
+#For each list element get current element to iterate the list from this element on, set the cost and reverse the list
+    [*1..NumElm-1].each do |e|
+      i = e-1
       min = nil
       min_idx = nil
   
-      [*i..N-1].each do |j|
-        if min.nil? || min > l[j]
+#Iterate the list to find the min number and it's index
+      [*i..NumElm-1].each do |j|
+        if min.nil? || min > list[j]
           min_idx = j
-          min = l[j]
+          min = list[j]
         end
       end
   
       cost += min_idx-i+1
   
-      l = rev(l, i, min_idx)
+      list = reverse(list, i, min_idx)
     end
     puts "Case ##{t}: #{cost}"
 end
