0def0606f7ce3626ef8830a7a31266dff7c8d446
Eduardo Isai Martínez Méndez <44093701+EduardoMtz1@users.noreply.github.com>
2022-12-23 16:51:54 -0600

    Rename: d1000000.java

--- solutions/d1000000/d1000000.java
+++ solutions/d1000000/d1000000.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
-public class Dice{
+public class d1000000{
     static Scanner inputObject = new Scanner(System.in); //Scaner for the input
     public static void main(String[] args) {
         int T = inputObject.nextInt(); //Read T = Number of cases
