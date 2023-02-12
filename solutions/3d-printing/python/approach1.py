9a20127574a1b6138f2b308b0cc98a26b7b39880
Aldo Santiago <aldo11santiago@gmail.com>
2023-02-12 12:54:32 -0600

    doc: updated documentation

--- solutions/3d-printing/python/approach1.py
+++ solutions/3d-printing/python/approach1.py
@@ -1,9 +1,33 @@
-def solve(printers: list) -> str:
-    """
-    finds a solution consisting of 4 numbers representing cyan, yellow, magenta, and black ink units
-    printers : list of (list of int)
-               inner lists represent ink units available in a printer, outer list represents the printers
-               [[c, y, m, b], [c, y, m, b], ...]
+"""3d printing solution
+
+This is a solution for the Google Code Jam '3d printing' problem
+The first line of the input gives the number of test cases, T.
+T test cases follow. Each test case consists of 3 lines.
+The i-th line of a test case contains 4 integers
+Ci, Mi, Yi, and Ki, representing the number of ink units
+in the i-th printer's cartridge for the colors
+cyan, magenta, yellow, and black, respectively.
+More information:
+codingcompetitions.withgoogle.com/
+    codejam/round/0000000000876ff1/0000000000a4672b
+"""
+
+def solve(printers):
+    """Finds a 4-digit color solution
+
+    Finds a solution consisting of 4 numbers
+    representing cyan, yellow, magenta, and black ink units
+    
+    Args:
+        printers: list of (list of int)
+            inner lists represent ink units available in a printer,
+            outer list represents the printers
+            [[c, y, m, b], [c, y, m, b], ...]
+        
+    Returns:
+        A string containing 4 space separated numbers
+        representing a color made up of
+        cyan, yellow, magenta and black ink units
     """
     
     # Initialize the "available ink units" per ink type
@@ -18,7 +42,8 @@ def solve(printers: list) -> str:
     inkUnits = sum(maxInkPerColor)
     
     if (inkUnits < 1000000):
-        # no combination can be used in EVERY printer to print with 1000000 units
+        # no combination can be used in EVERY printer
+        # to print with 1000000 units
         return "IMPOSSIBLE"
     
     for i in range(0, 4):
@@ -27,14 +52,15 @@ def solve(printers: list) -> str:
         if diff == 0:
             # return solution in string format
             return " ".join(str(item) for item in maxInkPerColor)
+        # if current sum is not 1000000 we need to reduce ink units
         if maxInkPerColor[i] >= diff:
-            # just need to reduce the diff and next iteration diff will be 0
+            # just need to reduce the diff
+            # and at next iteration diff value will be 0
             maxInkPerColor[i] -= diff
         else:
             maxInkPerColor[i] = 0
 
-# read number of test cases
-t = int(input())
+t = int(input()) # read number of test cases
 for i in range(1, t+1):
     printer1 = [int(s) for s in input().split(' ')]
     printer2 = [int(s) for s in input().split(' ')]
